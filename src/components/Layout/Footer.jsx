24c0cfebd7111b65ae017dd789bcbd66c5ea0b77
Khriztian9 <carvajalchr@gmail.com>
2025-09-28 09:37:57 -0500

    L

--- src/components/Layout/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-dark text-white text-center py-3 mt-4">
-      <p>© {new Date().getFullYear()} Metalcortes</p>
-    </footer>
-  );
-}
-export default Footer;
